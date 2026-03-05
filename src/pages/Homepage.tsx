e48a1ccd78e591cf77fa28b51bb0128b74fb1e37
Leonard Samuel Setiawan <leonsamuels080306@gmail.com>
2026-03-05 21:25:42 +0700

    finish template

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,6 +1,12 @@
 import { Box } from "@mui/material";
+import { useMenu } from "../hooks/useMenu";
 
 export default function Homepage() {
+    const {menuList, reload} = useMenu();
+
+    reload();
+    console.log(menuList);
+
     return <Box>
         <h1>Homepage</h1>
     </Box>
